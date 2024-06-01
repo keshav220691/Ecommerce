e3a39104d0e3bf0d06283662524fc97ddaf05671
Keshav Sharma <keshav.22cse@bmu.edu.in>
2024-06-01 16:51:29 +0530

    sidebar menu added

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ document.querySelectorAll('.frame').forEach(frame => {
         frame.scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+function showSidebar()
+{
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display="flex";
+}
+function hideSidebar()
+{
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display="none";
+}
